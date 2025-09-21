807cfb4697a877c546d2f41f7cf24d8d08a94c24
Harshit Singh Parmar <hsp9696@gmail.com>
2025-09-21 14:11:59 +0530

    Displaying Genres

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,17 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
 
       <Show above="md">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
