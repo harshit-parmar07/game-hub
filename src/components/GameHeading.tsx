5406f0915716db2cd57ad2eb1137a688d0941b70
Harshit Singh Parmar <hsp9696@gmail.com>
2025-10-04 22:35:04 +0530

    Dynamic heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,19 @@
+import { GameQuery } from "@/App";
+import { Heading } from "@chakra-ui/react";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    <Heading marginY={5} fontSize="5xl" as="h1">
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
